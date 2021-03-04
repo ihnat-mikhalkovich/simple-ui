110ad7cd6d9c2040b27cfe5e5432cde82418c8f8
Ihnat Mikhalkovich <ihnat.mikhalkovich@gmail.com>
2021-03-04 18:11:54 +0300

    First commit

---
+++ src/main/java/com/ihnat/mikhalkovich/simpleview/SimpleViewApplication.java
@@ -0,0 +1,13 @@
+package com.ihnat.mikhalkovich.simpleview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihnat/mikhalkovich/simpleview/SimpleViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihnat.mikhalkovich.simpleview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
